697e1116e9dee8ea543c881f9f6903b7796ad0ea
io-abhishek2206 <abhishekjain00e@gmail.com>
2025-11-06 01:35:32 +0530

    Initial Commit

---
+++ Main_File.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+bool algorithm1(bool opponents_last_choice)
+{
+    return //bool;
+}
+bool algorithm2(bool opponents_last_choice)
+{
+    return //bool;
+}
+int main()
+{
+    long int total_number_of_iterations=0;
+    cout<<"Enter the number of iterations in this test case : ";
+    cin>>total_number_of_iterations;
+}
